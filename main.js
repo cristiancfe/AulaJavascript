16c7c09ffac2dbf39eb98234e69d863792658874
cristiancfe <cristiancfe@gmail.com>
2021-09-18 23:43:36 -0300

    variáveis em Javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+var welcome = 'hello Javascript - Automação Full Stack from QA Ninja'
+document.getElementById('welcome').append(welcome)
+console.log(welcome)
+
+// variável  local para armazenar VALORES QUE PODEM SER ALTERADOS
+
+//tipagem dinâmica atribui valor quando recebe o valor
+
+var number = 10
+console.log(number)
+
+//typeof  responsável por trazer o tipo da variável
+console.log(typeof number)
+
+var nome = 'Cristian'
+console.log(typeof nome)
+
+var n1 = 10
+var n2 = 20
+console.log(n1 + n2)
