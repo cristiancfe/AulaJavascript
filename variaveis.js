e1da351acf6100d9eebdb56d0219a9f2107b7bbe
cristiancfe <cristiancfe@gmail.com>
2021-09-18 23:48:51 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,16 @@
+// variável  local para armazenar VALORES QUE PODEM SER ALTERADOS
+
+//tipagem dinâmica atribui valor quando recebe o valor
+
+var number = 10
+console.log(number)
+
+//typeof  responsável por trazer o tipo da variável
+console.log(typeof number)
+
+var nome = 'Cristian'
+console.log(typeof nome)
+
+var n1 = 10
+var n2 = 20
+console.log(n1 + n2)
