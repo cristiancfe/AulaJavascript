c890bcf8bf5c27826613cec4da46536d8a6fbb2a
cristiancfe <cristiancfe@gmail.com>
2021-09-20 14:30:00 -0300

    operdadores

--- Operadores.js
+++ Operadores.js
@@ -1,10 +1,19 @@
 n1 = 50
 n2 = 2
+n3 = 'cristian'
+n4 = '2'
+console.log('operadores matemáticos')
 console.log(n1 + n2) //soma
 console.log(n1 - n2) // subtração
 console.log(n1 * n2) // multiplicação
 console.log(n1 / n2) // divisão
 console.log(n1 ** n2) // exponenciação
 console.log(n1 % n2) // mod ou resto da divisão
+console.log('operadores condicionais')
+
 console.log(n1 > n2) // booleano tras valor true para verdadeiro
 console.log(n1 < n2) // booleano tras valor false para falso
+console.log(n1 == n2) // compara se o valor é igual
+console.log(n1 != n2) // compara se o valor é diferente
+console.log(n4 == n2) // compara se o valor é igual mas não compara o tipo , então dá um falso true
+console.log(n4 === n2) // compara se o valor  e o tipo são iguais, chamado de comparador absoluto
