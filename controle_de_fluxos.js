06801e398e33b1419c3660bc50539ccbbb5dcf98
cristiancfe <cristiancfe@gmail.com>
2021-09-20 16:30:58 -0300

    controle de fluxos

---
+++ controle_de_fluxos.js
@@ -0,0 +1,32 @@
+// if usado para tomada de decisão se(If)o valor for igual a 18
+// var idade = 18
+// if (idade >= 18) {
+//     console.log('você já pode tirar a carteira de habilitação')
+// }
+
+// votação
+// var idade = 18
+// if (idade >= 18 && idade < 65) {
+//     console.log('Voto obrigatório')
+// } else if (idade >= 16 && idade < 18) {
+//     console.log('voto opcional')
+// } else if (idade < 16) {
+//     console.log('Não pode votar ')
+// } else {
+//     console.log('Voto não é obrigatório')
+// }
+
+// estrutura de seleção usando switch case
+
+// var ingresso = 'premium'
+// switch (ingresso) {
+//     case 'vip':
+//         console.log('você ficará no camarote')
+//         break
+//     case 'premium':
+//         console.log('você ficará na Pista')
+//         break
+//     case 'comum':
+//         console.log('você ficará na arquibancada')
+//         break
+// }
