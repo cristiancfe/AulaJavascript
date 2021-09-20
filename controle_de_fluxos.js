9e9c9b7e4dfc792e764857e62d89e79578f67558
cristiancfe <cristiancfe@gmail.com>
2021-09-20 17:03:21 -0300

    controle de fluxos

--- controle_de_fluxos.js
+++ controle_de_fluxos.js
@@ -18,18 +18,37 @@
 
 // estrutura de seleção usando switch case
 
-var ingresso = 'premium'
-switch (ingresso) {
-    case 'vip':
-        console.log('você ficará no camarote')
-        break
-    case 'premium':
-        console.log('você ficará na Pista')
-        break
-    case 'comum':
-        console.log('você ficará na arquibancada')
-        break
-    default:
-        console.log('Tipo de ingresso inválido')
-        break
-}
+// var ingresso = 'premium'
+// switch (ingresso) {
+//     case 'vip':
+//         console.log('você ficará no camarote')
+//         break
+//     case 'premium':
+//         console.log('você ficará na Pista')
+//         break
+//     case 'comum':
+//         console.log('você ficará na arquibancada')
+//         break
+//     default:
+//         console.log('Tipo de ingresso inválido')
+//         break
+// }
+
+// laços de repetição com while ,For e foreach
+
+//criando um loop infinito com while
+// while (true) {
+//     console.log('Sou lindo!')
+// }
+// criando laço while, ele executa enquanto for verdadeira
+// var i = 0
+// while (i <= 10) {
+//     console.log('repetindo porque  ' + i + ' é menor ou igual a 10 ')
+//     i++
+// }
+
+//se não colocar i++ também entra em loop infinito
+// for (var a = 0; a < 10; a++) {
+//     console.log('Repetindo porque ' + a + ' é menor que 10')
+// }
+// pode se usar ${a} para concatenar a variável
