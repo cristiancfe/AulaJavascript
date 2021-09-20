56af982e6c6c10758daad223731b4a6de359f7c8
cristiancfe <cristiancfe@gmail.com>
2021-09-20 14:17:46 -0300

    operadores

---
+++ Operadores.js
@@ -0,0 +1,8 @@
+n1 = 50
+n2 = 2
+console.log(n1 + n2) //soma
+console.log(n1 - n2) // subtração
+console.log(n1 * n2) // multiplicação
+console.log(n1 / n2) // divisão
+console.log(n1 ** n2) // exponenciação
+console.log(n1 % n2) // mod ou resto da divisão
--- variaveis.js
+++ variaveis.js
@@ -13,3 +13,4 @@ console.log(typeof nome)
 
 var n1 = 10
 var n2 = 20
+console.log(n1 + n2)
