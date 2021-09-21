35339c0af4bc0ba0e8e5b1f088a679043226fa47
cristiancfe <cristiancfe@gmail.com>
2021-09-21 17:53:34 -0300

    trabalhando com objetos

--- objetos.js
+++ objetos.js
@@ -1,29 +1,29 @@
-// var album = {
-//     titulo: 'Black album',
-//     lançamento: 1991,
-//     showInfo: function() {
-//         console.log(
-//             'Título do album: ' + this.titulo + ' - lançado em: ' + this.lançamento
-//         )
-//     }
-// }
+var album = {
+    titulo: 'Black album',
+    lançamento: 1991,
+    showInfo: function() {
+        console.log(
+            'Título do album: ' + this.titulo + '- lançado em: ' + this.lançamento
+        )
+    }
+}
 
 // outra maneira de declarar o objeto com suas propriedades
-var album = new Object()
+//var album = new Object()
 
-album.titulo = 'black album'
-album.lançamento = 1991
+//album.titulo = 'black album'
+//album.lançamento = 1991
 
 //album.showInfo() // chama a função dentro do objeto
 console.log(album.titulo) //chama o titulo do album
 console.log(album.lançamento) // chama o ano de lançamento do album
 
-album.track = ['musica1', 'musica2', 'musica3']
-console.log(album.track)
+// album.track = ['musica1', 'musica2', 'musica3']
+// console.log(album.track)
 
-album.showtracks = function() {
-    this.track.forEach(function(value, key) {
-        console.log(value)
-    })
-}
-album.showtracks()
+// album.showtracks = function() {
+//     this.track.forEach(function(value, key) {
+//         console.log(value)
+//     })
+// }
+// album.showtracks()
