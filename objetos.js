d1ead9ae1c17691649094ecde3454d44a41593f7
cristiancfe <cristiancfe@gmail.com>
2021-09-21 17:29:04 -0300

    trabalhando com objetos

--- objetos.js
+++ objetos.js
@@ -1,13 +1,22 @@
-var album = {
-    titulo: 'Black album',
-    lançamento: 1991,
-    showInfo: function() {
-        console.log(
-            'Título do album: ' + this.titulo + ' - lançado em: ' + this.lançamento
-        )
-    }
-}
+// var album = {
+//     titulo: 'Black album',
+//     lançamento: 1991,
+//     showInfo: function() {
+//         console.log(
+//             'Título do album: ' + this.titulo + ' - lançado em: ' + this.lançamento
+//         )
+//     }
+// }
 
-album.showInfo() // chma aa função dentro do objetp
+// outra maneira de declarar o objeto com suas propriedades
+var album = new Object()
+
+album.titulo = 'black album'
+album.lançamento = 1991
+
+//album.showInfo() // chama a função dentro do objeto
 console.log(album.titulo) //chama o titulo do album
 console.log(album.lançamento) // chama o ano de lançamento do album
+
+album.track = ['musica1', 'musica2', 'musica3']
+console.log(album.track)
